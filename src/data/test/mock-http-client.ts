18c99887706afa13f010abfc83f1782dc726541e
nothingrotf <enderbr4@gmail.com>
2023-09-25 18:13:17 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct values

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -2,7 +2,9 @@ import type { HttpPostClient, HttpPostParams } from 'data/protocols/http/http-po
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
+  body?: object
   async post (params: HttpPostParams): Promise<void> {
     this.url = params.url
+    this.body = params.body
   }
 }
