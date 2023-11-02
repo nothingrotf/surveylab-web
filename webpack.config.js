c4b93b9b2fae7354c790885c86f3e76d7f922c13
nothingrotf <enderbr4@gmail.com>
2023-11-02 09:55:16 -0300

    fix: webpack invalid configurations object

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,12 @@ module.exports = {
     }]
   },
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    devMiddleware: {
+      writeToDisk: true
+    },
     historyApiFallback: true
   },
   externals: {
