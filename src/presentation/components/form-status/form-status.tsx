c79210bb496fe7a3b3a194ad26b4998506ec822d
nothingrotf <enderbr4@gmail.com>
2023-11-14 10:54:04 -0300

    feat: ensure login view will not render error on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './form-status-styles.scss'
+import Context from '@/presentation/contexts/forms/form-context'
 
 export const FormStatus: React.FC = () => {
+  const { errorMessage } = useContext(Context)
   return (
-    <div className={Styles.errorWrap}>
-      <span className={Styles.error}>Error</span>
+    <div data-testid='error-wrap' className={Styles.errorWrap}>
+      { errorMessage ?? <span className={Styles.error}>{errorMessage}</span> }
     </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,24 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './login-styles.scss'
 import { Logo, InputWrap, Button, Checkbox, Anchor, LoginHeader, FormStatus } from '@/presentation/components'
+import Context from '@/presentation/contexts/forms/form-context'
+
+type StateProps = {
+  errorMessage: string
+}
 
 export const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    errorMessage: ''
+  })
   return (
     <div className={Styles.login}>
       <section>
         <Logo />
         <div className={Styles.container}>
-          <form>
-            <LoginHeader />
-            <InputWrap type='email' name='email' placeholder='Enter your email' required label='Email' />
-            <InputWrap type='password' name='password' placeholder='••••••••' label='Password' />
-            <div className={Styles.rememberForgot}>
-              <Checkbox htmlFor='remember-forgot' name='remember-forgot' id='remember-forgot'>Remember me</Checkbox>
-              <Anchor href='#'>Forgot password?</Anchor>
-            </div>
-            <Button type='submit'>Login</Button>
-            <FormStatus />
-          </form>
+          <Context.Provider value={state}>
+            <form>
+              <LoginHeader />
+              <InputWrap type='email' name='email' placeholder='Enter your email' required label='Email' />
+              <InputWrap type='password' name='password' placeholder='••••••••' label='Password' />
+              <div className={Styles.rememberForgot}>
+                <Checkbox htmlFor='remember-forgot' name='remember-forgot' id='remember-forgot'>Remember me</Checkbox>
+                <Anchor href='#'>Forgot password?</Anchor>
+              </div>
+              <Button type='submit'>Login</Button>
+              <FormStatus />
+            </form>
+          </Context.Provider>
         </div>
         <p>Don&apos;t have an account? <Anchor href='#'>Sign up</Anchor></p>
       </section>
