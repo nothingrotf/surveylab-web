bd786b2fad7e9d1047cd7b1c4d55e718d79e4ae6
nothingrotf <enderbr4@gmail.com>
2023-11-15 09:45:16 -0300

    test: ensure login view calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
-import { type RenderResult, render } from '@testing-library/react'
+import { type RenderResult, render, fireEvent, cleanup } from '@testing-library/react'
 import Login from './login'
+import type { Validation } from '@/presentation/protocols/validation'
+
+const mockValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    errorMessage: string
+    validate (input: object): string {
+      return this.errorMessage
+    }
+  }
+  return new ValidationStub()
+}
 
 type SutTypes = {
   sut: RenderResult
+  validationStub: Validation
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
-  return { sut }
+  const validationStub = mockValidation()
+  const sut = render(<Login validation={validationStub} />)
+  return { sut, validationStub }
 }
 
 describe('Login Component', () => {
+  afterEach(cleanup)
+
   test('Should start wih initial state', () => {
     const { sut } = makeSut()
     const errorWrap = sut.getByTestId('error-wrap')
@@ -22,5 +37,15 @@ describe('Login Component', () => {
     expect(emailStatus.childElementCount).toBe(2)
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.childElementCount).toBe(2)
+  })
+
+  test('Should call Validation with correct email', () => {
+    const { sut, validationStub } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+    fireEvent.input(emailInput, { target: { value: 'any_email' } })
+    expect(validateSpy).toHaveBeenCalledWith({
+      email: 'any_email'
+    })
   })
 })
