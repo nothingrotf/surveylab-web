1fab35fc6243c42c728b9ff07dd4cd4f775a58f6
nothingrotf <enderbr4@gmail.com>
2023-11-15 11:06:48 -0300

    feat: ensure login view present error if password is invalid

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -1,13 +1,9 @@
 import type { Validation } from '@/presentation/protocols/validation'
 
-export class ValidationSpy implements Validation {
-  fieldName: string
-  fieldValue: string
+export class ValidationStub implements Validation {
   errorMessage: string
 
   validate (fieldName: string, fieldValue: string): string {
-    this.fieldName = fieldName
-    this.fieldValue = fieldValue
     return this.errorMessage
   }
 }
