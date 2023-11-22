23b2c97064d3757d02c966ad495980bceb0e09e8
nothingrotf <enderbr4@gmail.com>
2023-11-22 11:37:11 -0300

    feat: ensure MinLengthValidation return error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -0,0 +1,10 @@
+import type { FieldValidation } from '@/validation/protocols'
+import { InvalidFieldError } from '@/validation/errors'
+
+export class MinLengthValidation implements FieldValidation {
+  constructor (readonly field: string, private readonly minLength: number) {}
+
+  validate (value: string): Error {
+    return new InvalidFieldError(this.field)
+  }
+}
